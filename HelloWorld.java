3afb01e16908e1ecafdbae593dc643937f7bfbcf
mrsolankigit <mrsolanki1903@gmail.com>
2023-12-25 17:46:51 +0530

    String concatenation

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,9 @@ class HelloWorld
 public static void main(String args[])
 {
   System.out.println("Hello Friends");
+  String s1="GitHub";
+  String s2="Practice Demo";
+  System.out.println(s1+s2);
 
 }
 }
